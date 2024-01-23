b2ee5d6c1a29b51dfb3da679fb28d0b62c2fbefb
Russell Selph <rselph@users.noreply.github.com>
2024-01-22 22:26:41 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-files=$(curl https://www.backblaze.com/b2/hard-drive-test-data.html 2>/dev/null | egrep --only-matching https://[^\"]+data_[[:alnum:]_]+\\.zip)
+files=$(curl https://www.backblaze.com/cloud-storage/resources/hard-drive-test-data 2>/dev/null | egrep --only-matching 'https://[^\"]+data_[[:alnum:]_]+\.zip')
 for file in ${files} ; do
     echo $(basename ${file})
-    [[ -f $(basename ${file}) ]] || wget ${file}
+    [[ -f $(basename ${file}) ]] || curl -O ${file}
 done
